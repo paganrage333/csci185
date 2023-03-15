031b2f94a8450a051347817d0239caa32f5878f9
page <aragan@unca.edu>
2023-03-15 12:15:09 -0400

    lecture 16

---
+++ lectures/lecture16/01-warmup/index.js
@@ -0,0 +1,16 @@
+function toggleFall() {
+    console.log('change theme to fall')
+    document.querySelector('body').className = 'fall';
+}
+
+function toggleWinter() {
+    document.querySelector('body').className = 'winter';
+}
+
+function toggleSpring() {
+    document.querySelector('body').className = 'spring';
+}
+
+function toggleUNCA() {
+    document.querySelector('body').className = 'unca';
+}
