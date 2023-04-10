8c371753984f28689db3e771afb777150a56b79f
page <aragan@unca.edu>
2023-04-10 16:51:08 -0400

    started hw6

---
+++ homework/hw06/carousel.js
@@ -0,0 +1,52 @@
+const photos = [
+    "images/img1-600x400.jpg",
+    "images/img2-600x400.jpg",
+    "images/img3-600x400.jpg",
+    "images/img4-600x400.jpg",
+    "images/img5-600x400.jpg",
+    "images/img6-600x400.jpg",
+    "images/img7-600x400.jpg",
+    "images/img8-600x400.jpg",
+    "images/img9-600x400.jpg",
+    "images/img10-600x400.jpg"
+];
+const elem = document.querySelector(".current");
+let idx = 0;
+
+
+/* This function should:
+    (1) display the new image inside of the .current-photo container, and 
+    (2) update the caption inside of the .caption paragraph
+*/
+function showImage(elem) {
+    console.log('Show image');
+    // elem.src =[idx];
+}
+
+
+/* This function should set the idx variable 
+   to one greater than the current value of idx, 
+   and then invoke the showImage() function.
+   If the idx gets to one less than the length of 
+   the array, set idx to 0.
+*/
+function forward() {
+    console.log('forward');
+    for (let idx=0; idx < photos.length; idx++) {
+    showImage();
+    }
+}
+
+
+/* This function should set the idx variable 
+   to one less than the current value of idx, 
+   and then invoke the showImage() function.
+   If the idx gets to the beginning, set idx to
+   one less than the length of the array.
+*/
+function back() {
+    console.log('back');
+    for (let idx=0; idx < photos.length; idx--) {
+        showImage();
+        }
+}
